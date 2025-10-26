cbf0fdcd7d83d606e2278983912d89b5d0386771
chatgate2023 <support@chatgate.io>
2025-10-26 12:39:16 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
+// ضع هنا رابط Web App الخاص بك
 const webAppUrl = "https://script.google.com/macros/s/AKfycbwxRdUh8HBoCudTlEVVd0de-luk8fD90x3NY4c6aykVPrzNFuHnXuTUm87vdyrSWInt/exec";
+
+// ضع هنا Spreadsheet ID الخاص بك
 const spreadSheetId = "1x3-O0ZVHczozdymG--EOD_Gqy-Tgn7FvpjGgWSIolHE";
 
 async function fetchData() {
